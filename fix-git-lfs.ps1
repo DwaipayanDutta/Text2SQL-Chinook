e0ab021426c32bdad6fbcf96eba6e1682be2f90f
Dwaipayan <dwaipayandutt@gmail.com>
2026-01-31 23:34:01 +0530

    Create fix-git-lfs.ps1

---
+++ fix-git-lfs.ps1
@@ -0,0 +1,18 @@
+Write-Host "🔧 Initializing Git LFS."
+git lfs install
+
+Write-Host "Increasing Git HTTP buffer and disabling compression."
+git config --global http.postBuffer 524288000
+git config --global core.compression 0
+
+Write-Host "Current LFS tracked files:"
+git lfs track
+git lfs ls-files
+
+Write-Host "Forcing upload of all Git LFS objects."
+git lfs push --all origin main
+
+Write-Host "Retrying normal git push"
+git push origin main
+
+Write-Host " Done."
